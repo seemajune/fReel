58cca6c74a7b2fa21af26fddb78997923b259120
seemajune <seemashariat@gmail.com>
2016-04-27 17:35:08 -0400

    adds modal across width of screen in fixed pos

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
       for(var i =0; i < _data.items.length; i++){
         $fanReel.append('<div class="thumbnail" id=' + _data.items[i].id + '><img src=' + 'http:' + _data.items[i].photo.medium_square.url +
          '><div class="hover-overlay no-display">username: ' + _data.items[i].user.username + '<br />like count: ' + _data.items[i].like_count + 
-         '</div><div class="triangle-overlay no-display"><img src="triangle.png">');
+         '</div>');
       }
 
       var $thumbnail = $('.thumbnail');
@@ -31,16 +31,16 @@ $(document).ready(function(){
 
         $('.outer-modal').remove();
 
-        var lengthOfImages = $('.triangle-overlay').length,
+        var lengthOfImages = $('.thumbnail').length,
           productImgUrl,
           productLink;
           // calculate width of screen / ($(e.currentTarget).width()) & get index of to find out how many images are in row, insert modal after index of last in that row;
         $(e.currentTarget).children().eq(1).addClass('no-display');
 
         for(var i=0; i<lengthOfImages; i++){
-          if (!$('.triangle-overlay').eq(i).hasClass('no-display')){
-            $('.triangle-overlay').eq(i).addClass('no-display');
-          } 
+          // if (!$('.triangle-overlay').eq(i).hasClass('no-display')){
+          //   $('.triangle-overlay').eq(i).addClass('no-display');
+          // } 
           if ($('.outer-modal').eq(i).attr('product-data') !== $(e.currentTarget).attr('id')){
             $('.outer-modal').eq(i).remove();
           } 
@@ -53,7 +53,7 @@ $(document).ready(function(){
 
         $(e.currentTarget).children().eq(2).removeClass('no-display');
 
-        $('<div class="outer-modal" product-data="' + $(e.currentTarget).attr('id') +'">' + '<a href="'+ productLink + '"><img src="http:'+ productImgUrl + '"></a></div>').insertAfter($(e.currentTarget));
+        $fanReel.append('<div class="outer-modal" product-data="' + $(e.currentTarget).attr('id') +'">' + '<a href="'+ productLink + '"><img src="http:'+ productImgUrl + '"></a></div>');
         
       });
     }
